1a8fbb4a89ddcf90b0ceaac00b36aba965529c84
shikhor <shikhor.roy@cefalo.com>
2025-03-29 15:26:58 +0600

    complete a task

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -11,8 +11,8 @@ import {TaskComponent} from "./task/task.component";
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
-  @Input({required: true}) userId !: string
-  @Input({required: true}) name !: string
+  @Input({required: true}) userId !: string;
+  @Input({required: true}) name !: string;
   tasks = [
     {
       id: 't1',
@@ -20,14 +20,14 @@ export class TasksComponent {
       title: 'Master Angular',
       summary:
         'Learn all the basic and advanced features of Angular & how to apply them.',
-      dueDate: '2025-12-31',
+      dueDate: '2025-12-31'
     },
     {
       id: 't2',
       userId: 'u3',
       title: 'Build first prototype',
       summary: 'Build a first prototype of the online shop website',
-      dueDate: '2024-05-31',
+      dueDate: '2024-05-31'
     },
     {
       id: 't3',
@@ -35,11 +35,18 @@ export class TasksComponent {
       title: 'Prepare issue template',
       summary:
         'Prepare and describe an issue template which will help with project management',
-      dueDate: '2024-06-15',
-    },
-  ]
+      dueDate: '2024-06-15'
+    }
+  ];
 
   get selectedUserTasks() {
-    return this.tasks.filter(task => task.userId == this.userId)
+    return this.tasks.filter(task => task.userId == this.userId);
+  }
+
+  completedTask(taskId: string) {
+    const index = this.tasks.findIndex(task => task.id == taskId);
+    if (index !== -1) {
+      this.tasks.splice(index, 1);
+    }
   }
 }
